30a78d974584e6a911e8ac35f426879b0133ef18
BJ Herrington <bob@paradoxlost.com>
2014-03-18 23:36:52 -0500

    Optional output file

--- smali2java/Program.cs
+++ smali2java/Program.cs
@@ -14,17 +14,27 @@ namespace Smali2Java
                 Console.WriteLine("ERROR: No Input File! using sample file\n");
 
             String sFile = args.Length > 0 ? args[0] : "Samples\\AndroidHandler.smali";
+            string destFile = args.Length > 1 ? args[1] : null;
 
-            Console.BufferHeight = 10000;
-            Console.WindowWidth = Console.LargestWindowWidth;
-            Console.BufferWidth = Console.LargestWindowWidth;
-
-            Console.WriteLine("Decompiling file: " + sFile);
-            Console.WriteLine("[");
             SmaliEngine e = new SmaliEngine();
-            Console.Write(e.Indent(e.Decompile(sFile)));
-            Console.WriteLine("]");
-            Console.ReadKey();
+            string contents = e.Indent(e.Decompile(sFile));
+            if (string.IsNullOrEmpty(destFile))
+            {
+                Console.BufferHeight = 10000;
+                Console.WindowWidth = Console.LargestWindowWidth;
+                Console.BufferWidth = Console.LargestWindowWidth;
+
+                Console.WriteLine("Decompiling file: " + sFile);
+                Console.WriteLine("[");
+
+                Console.Write(contents);
+                Console.WriteLine("]");
+                Console.ReadKey();
+            }
+            else
+            {
+                System.IO.File.WriteAllText(destFile, contents);
+            }
         }
     }
 }
